39969ef83fecda0de63b49d7f6c9b4ad55175ae5
ijse <i@ijser.cn>
2013-10-28 17:41:14 +0800

    Re-caculate page height

--- js/action.js
+++ js/action.js
@@ -3,17 +3,27 @@ $(function() {
 	
 	// Global Status
 	var nowPage = 0; // [ 0, 1, 2, 3 ]
-	var partHeight = document.documentElement.clientHeight - 37;
+	var partHeight = 855; 
+	
+	
+	var initPageHeight = function() {
+		partHeight = document.documentElement.clientHeight - 37;
+		// Min height 855px each page
+		partHeight = partHeight < 855 ? 855 : partHeight;
+	};
+	initPageHeight();
+	window.onresize = initPageHeight;
+	
 	
 	var scrollPage = null;
 	
 	// Logo slip left at start
-	TweenLite.to($("#logo"), 1, { "margin-left":"0", opacity: 1 });
+	TweenLite.to($("#logo"), 0.5, { "margin-left":"0", opacity: 1 });
 
 	// TODO: Adjust by screen height
 	$(".bg").height(partHeight);
 	
-	// TODO: Bind Arrow keys
+	// TODO: Bind arrow keys
 	
 	// Trigger by mousewheel
 	$("body").mousewheel(function(event, delta, deltaX, deltaY) {
@@ -21,7 +31,7 @@ $(function() {
 		// Ensure step only one
 		nowPage -= delta > 0 ? 1 : -1;
 		
-		// Range 0-3 ensurance
+		// Ensure range 0-3
 		nowPage = nowPage < 0 ? 0 : nowPage;
 		nowPage = nowPage > 3 ? 3 : nowPage;
 		
