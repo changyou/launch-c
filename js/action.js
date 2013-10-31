c56ac7557b15e9216114f50bc4ae4732c24d390d
ijse <i@ijser.cn>
2013-10-31 20:14:58 +0800

    update

--- js/action.js
+++ js/action.js
@@ -9,8 +9,9 @@ $(function() {
 	
 	var $body = $("body"),
 		$logo = $("#logo"),
-		$phone0 = $("#phone0"),
+		$intro1 = $(".st_0 .intro-text"),
 		$appicons = $("#appicons"),
+		$phone0 = $("#phone0"),
 		$intro2 = $("#intro2"),
 		$phone1 = $("#wphone"),
 		$functions = $("#functions"),
@@ -21,21 +22,26 @@ $(function() {
 	Tween.from($logo, 0.5, { "margin-left":"100px", opacity: 1 });
 	window.steps = [
 		function step0() {
+			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[0] * partHeight), delay: 0.5, ease: Sine.easeOut });
 			Tween.to($body, 0.5, { "scrollTop": 0 });
 			Tween.to($appicons, 1, { top: "0" });
-			Tween.to($phone0, 1, { opacity: 1, top: "200px", yoyo: true });
+			Tween.to($intro1, 1, { top: "200px" });
+			Tween.to($phone0, 1, { top: "200px", yoyo: true });
 		},
 		function step1() {
-			Tween.to($appicons, 0.2, { top: "-500px" });
-			Tween.to($phone0, 2, { opacity: 0, top: -partHeight + "px"});
+			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[1] * partHeight), delay: 0.5, ease: Sine.easeInOut });
+			Tween.to($appicons, 1, { top: "-=500px" });
+			Tween.to($intro1, 1, { top: "-=500px" });
+			Tween.to($phone0, 1, { top: "-=475px", delay: 0.5});
 			Tween.to($st2, 1, { opacity: 1, yoyo: true });
-			Tween.to($functions, 2, { opacity: 0.8, top: "50%", "margin-top": "-320px" });
+			Tween.to($functions, 2, { opacity: 0.8, top: "50%", marginTop: "-320px" });
 			Tween.to($intro2, 1, { opacity: 0, yoyo: true, top: "100%" });
 			
 			Tween.to($wheatear, 1, { opacity: 1, bottom: 0 });
-			Tween.to($polygon, 1, { opacity: 0, top:"100%", "margin-top": "-250px" });
+			Tween.to($polygon, 1, { opacity: 0, top:"100%", marginTop: "-250px" });
 		},
 		function step2() {
+			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[2] * partHeight), delay: 0.5, ease: Sine.easeOut });
 			Tween.to($st2, 1, { opacity: 0.8, yoyo: true });
 			Tween.to($functions, 2, { opacity: 0, yoyo: true, "top": "-200px" });
 			Tween.to($phone1, 1, { opacity: 0, yoyo: true, skewX: "0" });
@@ -46,13 +52,15 @@ $(function() {
 			Tween.to( $(".wphone"), 2, { opacity: 0 });
 		},
 		function step3() {
+			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[3] * partHeight), delay: 0.5, ease: Sine.easeOut });
 			Tween.to($phone1, 1, { opacity: 1, yoyo: true, skewX: "-30deg", transformOrigin: "0% 100%" });
-			Tween.staggerTo( $(".wphone"), 2, { opacity: 1 }, 0.5);
+			Tween.staggerTo( $(".wphone"), 1.5, { opacity: 1, delay: 0.5 }, 0.5);
 			Tween.to($polygon, 1, { opacity: 1, top: "0%" });
 		},
 		function step4() {
+			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[4] * partHeight), delay: 0.5, ease: Sine.easeOut });
 			Tween.to( $(".wphone"), 2, { opacity: 0 });
-			Tween.to($polygon, 1, { opacity: 0, top: "-200%" });
+			Tween.to($polygon, 2, { opacity: 0, top: "-200%" });
 			Tween.to($phone1, 1, { opacity: 0, yoyo: true });
 		}
 	];
@@ -69,10 +77,13 @@ $(function() {
 	initPageHeight();
 	window.onresize = initPageHeight;
 	
-	
-	// TODO: Bind arrow keys
-	
 	var switchStage = function(n) {
+		// Scroll page smoothly
+		// $("body").scrollTop(pageDeltas[nowPage] * partHeight);
+//		TweenLite.to( $("body"), 1, { scrollTop: pageDeltas[nowPage] * partHeight, delay: 0.1 });
+//		TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[n] * partHeight), delay: 0.5, ease: Sine.easeOut });
+//		TweenLite.to( $("body"), 1, { scrollTo: { y: pageDeltas[nowPage] * partHeight }, delay: 0.1 });
+
 		//TODO: calculate the path to n-stage
 		if(steps[n]) {
 			steps[n]();
@@ -89,9 +100,6 @@ $(function() {
 		nowPage = nowPage < 0 ? 0 : nowPage;
 		nowPage = nowPage > 4 ? 4 : nowPage;
 		
-		// Scroll page smoothly
-		// $("body").scrollTop(pageDeltas[nowPage] * partHeight);
-		TweenLite.to( $("body"), 1, { scrollTop: pageDeltas[nowPage] * partHeight, delay: 0.1 });
 		switchStage(nowPage);
 		
 		return false;
