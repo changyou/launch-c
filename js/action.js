aa8380efd3d17fdcf598d9fed2f12b44e3456042
ijse <i@ijser.cn>
2013-11-28 14:22:31 +0800

    add lock when animating

--- js/action.js
+++ js/action.js
@@ -20,35 +20,39 @@ $(function() {
 		$wphone = $(".wphone-imgs"),
 		$intro3 = $("#intro3"),
 		$st2 = $(".st_2");
+	var flag = false; // in process
+	var unLock = function() {
+		flag = false;
+	};
 
 	Tween.from($logo, 0.5, { "margin-left":"100px", opacity: 1 });
 	window.steps = [
 		function step0() {
-			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[0] * partHeight), delay: 0.5, ease: Sine.easeOut });
+			Tween.to( $("body"), 1, { marginTop: -(pageDeltas[0] * partHeight), delay: 0.5, ease: Sine.easeOut });
 			Tween.to($body, 0.5, { "scrollTop": 0 });
 			Tween.to($appicons, 1, { top: "0" });
 			Tween.to($intro1, 1, { top: "200px" });
 			Tween.to($phone0, 1, { top: "200px", yoyo: true });
-			Tween.to($wheatear, 1, { bottom: "-610px", delay: 1 });
+			Tween.to($wheatear, 1, { bottom: "-610px", delay: 1, onComplete: unLock });
 		},
 		function step1() {
-			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[1] * partHeight), delay: 0.5, ease: Sine.easeInOut });
+			Tween.to( $("body"), 1, { marginTop: -(pageDeltas[1] * partHeight), delay: 0.5, ease: Sine.easeInOut });
 			Tween.to($appicons, 1, { top: "-=500px" });
 			Tween.to($intro1, 1, { top: "-=500px", delay: 1 });
 			Tween.to($phone0, 1, { top: "-=475px", delay: 0.5});
 			Tween.to($st2, 1, { opacity: 1, yoyo: true });
 			Tween.to($functions, 2, { opacity: 0.8, top: "50%", marginTop: "-260px" });
-			Tween.to($intro2, 1, { opacity: 0, yoyo: true, top: "100%" });
+			Tween.to($intro2, 1, { opacity: 0, yoyo: true, top: "100%", onComplete: unLock });
 
 			Tween.to($wheatear, 1, { opacity: 1, bottom: 0 });
 			Tween.to($polygon, 1, { opacity: 0, top:"100%", marginTop: "-210px" });
 		},
 		function step2() {
-			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[2] * partHeight), delay: 0.5, ease: Sine.easeOut });
+			Tween.to( $("body"), 1, { marginTop: -(pageDeltas[2] * partHeight), delay: 0.5, ease: Sine.easeOut });
 			Tween.to($st2, 1, { opacity: 0.8, yoyo: true });
 			Tween.to($functions, 2, { opacity: 0, yoyo: true, "top": "-200px" });
 			Tween.to($phone1, 1, { opacity: 0, yoyo: true, skewX: "0" });
-			Tween.to($intro2, 1, { opacity: 1, yoyo: true, top: "145px", delay: 1 });
+			Tween.to($intro2, 1, { opacity: 1, yoyo: true, top: "145px", delay: 1, onComplete: unLock });
 
 			Tween.to($wheatear, 1, { opacity: 0, bottom: "500px", yoyo: true });
 			Tween.to($polygon, 1, { opacity: 1, top: "0%" });
@@ -56,18 +60,18 @@ $(function() {
 			Tween.to($intro3, 1, { opacity: 0, top: "100%" });
 		},
 		function step3() {
-			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[3] * partHeight), delay: 0.5, ease: Sine.easeOut });
+			Tween.to( $("body"), 1, { marginTop: -(pageDeltas[3] * partHeight), delay: 0.5, ease: Sine.easeOut });
 			Tween.to($phone1, 1, { opacity: 1, yoyo: true, skewX: "-30deg", transformOrigin: "0% 100%" });
 			Tween.staggerTo( $(".wphone"), 1.5, { opacity: 1, delay: 0.5 }, 0.5);
 			Tween.to($wphone, 2, { top: "20px"});
 			Tween.to($polygon, 1, { opacity: 1, top: "0%" });
-			Tween.to($intro3, 1, { opacity: 1, top: "420px", delay: 1.5} );
+			Tween.to($intro3, 1, { opacity: 1, top: "420px", delay: 1.5, onComplete: unLock} );
 		},
 		function step4() {
-			TweenLite.to( $("body"), 1, { marginTop: -(pageDeltas[4] * partHeight), delay: 1, ease: Sine.easeOut });
+			Tween.to( $("body"), 1, { marginTop: -(pageDeltas[4] * partHeight), delay: 1, ease: Sine.easeOut });
 			Tween.to( $(".wphone"), 2, { opacity: 0, delay: 1 });
 			Tween.to($wphone, 2, { top: "-200%", delay: 0.5 });
-			Tween.to($polygon, 2, { opacity: 0, top: "-200%" });
+			Tween.to($polygon, 2, { opacity: 0, top: "-200%", onComplete: unLock });
 			Tween.to($phone1, 1, { opacity: 0, yoyo: true });
 		}
 	];
@@ -97,9 +101,14 @@ $(function() {
 		}
 	};
 
+
+
 	// Trigger by mousewheel
 	$("body").mousewheel(function(event, delta) {
-
+		if(flag) {
+			return false;
+		}
+		flag = true;
 		// Ensure step only one
 		nowPage -= delta > 0 ? 1 : -1;
 
