87da147883e3dc048b2737c97d03a3885f45b5e3
meterler <meterler@126.com>
2014-05-19 09:48:44 +0800

    6 channels

---
+++ main/election/scripts/index6.js
@@ -0,0 +1,56 @@
+var config = {
+  "apkName": ["BJP-political-party.apk",
+    "Congress-Party.apk",
+    "BJP.apk",
+    "Congress-Rahul-Gandh.apk",
+    "BJP-Narendra-Modi.apk",
+    "CPIM-Political-Party.apk",
+    "TMC-Mamta-Bannerjee.apk",
+    "SP-Samajwadi-Party.apk",
+    "BJP-Modi-Sarkar.apk"
+  ],
+  "baseUrl": "http://d.c-launcher.com/apk/0516/37007/",
+  "channel": "twitter"
+};
+
+function onload(img){
+  img.binded = true;
+  img.onload = function(){
+    this.style.opacity = '1';
+  };
+}
+
+/**
+ * selector [string]
+ */
+function loadimg(s){
+  var imglist = document.querySelectorAll(s),img;
+
+  for (var i = 0, len = imglist.length; i < len; i++) {
+    img = imglist[i];
+    img.src = img.dataset.src;
+    if(img.complete || img.binded){ continue; }
+    img.style.opacity = '0';
+    img.style.transition = 'opacity .5s';
+    onload(img);
+  }
+};
+
+function handleDownload(){
+  var downloadLinks = document.querySelectorAll(".j-download");
+  for (var i = 0, len = downloadLinks.length; i < len; i++) {
+    var num = i+1;
+    link = downloadLinks[i];
+    link.href=config.baseUrl+config.apkName[i];
+    link.onclick = function(){
+      _czc.push(﻿["_trackEvent","electionDownload at"+config.channel,"click","theme"+num,1]);
+      return true;
+    }
+  }
+}
+
+function loaded () {
+  handleDownload();
+  loadimg('.thum');
+}
+
